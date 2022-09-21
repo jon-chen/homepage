c6fd012a73912fd5d1cdc9037b03262303295106
Jonathan Chen <jonchen228@gmail.com>
2022-09-21 00:51:35 -0400

    update local build script to include deploy to swarm

--- local-build.sh
+++ local-build.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
-IMAGE_TAG=docker-hub.cynicsoft.net/homepage:docker-swarm
+set -ue
+
+WD=$(dirname $0)
+
+# Build image
+IMAGE_TAG=docker-hub.cynicsoft.net/homepage:drone-ci
 DOCKER_BUILDKIT=1 docker image build --tag $IMAGE_TAG .
+
+# Deploy
+docker push $IMAGE_TAG
+
+${WD}/../../swarm/conf/homepage-stack/homepage.deploy.sh
+docker service update --force homepage-swarm_homepage
